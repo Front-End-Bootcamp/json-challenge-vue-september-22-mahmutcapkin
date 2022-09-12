d7378a5c15ee69bb6a261c9b32cc5400a0082f5a
Mahmut Çapkın <mahmutcapkin@hotmail.com>
2022-09-12 08:58:15 +0300

    comment lines added

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,15 +3,31 @@ import axios from 'axios'
 async function getGroupMembers(){
   const result = await axios.get("./assets/data.json").then(res=>res.data);
 
-  let data = result.reduce((r, { group: group,assistant, ...value }) => {
-        var temp = r.find(o => o.group === group);
+	/*jsondaki verileri result'a aktardıktan sonra group bilgisine göre gruplamak için
+	* dataları reduce ile döndük.
+	*/
+  let data = result.reduce((newData, { group: group,assistant, ...value }) => {
+				/*reduce' un döneceği newData değeri içerisinde destructuring ile aldığımız verilerden group bilgisi
+				 var mı  diye kontrol ediyoruz. Eğer yoksa bu değeri reduce ile döneceğimiz objeye pushluyoruz.
+				 */
+        var temp = newData.find(o => o.group === group);
         if (!temp) 
-          r.push(temp = { group, student: [],assistant });
-        temp.student.push(value.name);
+					newData.push(temp = { group, student: [],assistant });
+				/*Buraya destructuring ile gelen veri her işlem sonunda bir sonraki veriye geçerek devam eder
+				/Yukarıda reduce ile dönecek olan newData içerisinde önceden olan bir group varsa
+				bunu sadece student kısmına pushluyoruz çünkü bu group bilgisi zaten 
+				eklenmiş ve bu kişi o grupta bulunan bir öğrenci
+				*/
+				/*Burada ise type bilgisi boş değilse bu kişinin assistant kısmına eklenmesi gerekiyor.
+				Eğer type bilgisi boş ise bu verinin student kısmına eklenir.
+				*/
         if(value.type!=null){
           temp.assistant =value.name;
-        }
-        return r;
+        }else{
+					temp.student.push(value.name);
+				}
+				// gelen veriler bitene kadar buradaki döngü devam eder.
+        return newData;
     }, []);
 		console.log(data);
 		return data;
@@ -19,11 +35,12 @@ async function getGroupMembers(){
 
 
 async function getFilterGroup(value){
+	//getGroupMembers() function'ını çağırıp içerisinden dönen gruplanmış veriyi kullanarak filtreleme işlemi yapıldı.
 	let result = await getGroupMembers().then(data=>data);
 	if(value)
 		console.log(result.filter(x=>x.group==value))
-	
 }
 
-getGroupMembers();
+getGroupMembers(); //Bu function'ı çağırmadan da tüm gruplanmış verileri getFilterGroup() ile console.log() 'a yazdırabiliriz.
+
 getFilterGroup("Orchid");
